765421af335c5be889e00565a9d468a652db7949
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-04-18 17:08:59 +0200

    Update Main.jsx

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,41 +1,51 @@
 import React, { useState } from "react";
 
 export default function Main() {
-    const [meme, setMeme] = useState({
-        topText: "One does not simply",
-        bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg"
-    });
+  const [meme, setMeme] = useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imageUrl: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input
-                        type="text"
-                        placeholder="Enter top text"
-                        name="topText"
-                        value={meme.topText}
-                        onChange={(e) => setMeme({ ...meme, topText: e.target.value })}
-                    />
-                </label>
+  function handleChange(event) {
+    const { name, value } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
 
-                <label>Bottom Text
-                    <input
-                        type="text"
-                        placeholder="Enter bottom text"
-                        name="bottomText"
-                        value={meme.bottomText}
-                        onChange={(e) => setMeme({ ...meme, bottomText: e.target.value })}
-                    />
-                </label>
-                <button>Get a new meme image 🖼</button>
-            </div>
-            <div className="meme">
-                <img src={meme.imageUrl} alt="Meme" />
-                <span className="top">{meme.topText}</span>
-                <span className="bottom">{meme.bottomText}</span>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="Enter top text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Enter bottom text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+        </label>
+        <button>Get a new meme image 🖼</button>
+      </div>
+      <div className="meme">
+        <img src={meme.imageUrl} alt="Meme" />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
 }
